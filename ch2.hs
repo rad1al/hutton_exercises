071a49888738fcf0c412670edb9a5316f90f1712
Your Name <charlzjin@gmail.com>
2017-08-13 07:31:46 -0400

    updated ch2.hs

--- ch2.hs
+++ ch2.hs
@@ -23,9 +23,11 @@
 
 {- 4. -}
 
+last' :: [a] -> a
 last' []  = error "empty list"
-last' xs  = drop ((length xs) - 1) xs
+last' xs  = xs !! ((length xs) - 1)
 
+last'' :: [a] -> a
 last'' []  = error "empty list"
 last'' xs  = head $ reverse xs
 
@@ -35,9 +37,12 @@ last''' [x] = x
 last''' (x:xs) = last''' xs
 
 
-{- 5. -}
+{- 5. 
 
 take (length-1) [1,2,3,4,5]
 take ((length [1,2,3,4,5])-1) [1,2,3,4,5]
 reverse (drop 1 (reverse [1,2,3,4,5]))
 (reverse . tail . reverse) [1,2,3,4,5]
+
+-}
+
