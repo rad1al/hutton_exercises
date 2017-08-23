5919bc44bca74c1dcd0b5f02fda91e509acbd1c1
Your Name <charlzjin@gmail.com>
2017-08-22 21:14:32 -0400

    updated notes_ch13.hs

--- notes_ch13.hs
+++ notes_ch13.hs
@@ -13,6 +13,7 @@
 
 import Control.Applicative
 import Data.Char
+import System.IO
 
 newtype Parser a = P (String -> [(a,String)])
 
@@ -422,6 +423,7 @@ enter expressions.
 
 -}
 
+
 box :: [String]
 box = ["+---------------+",
        "|               |",
@@ -442,6 +444,83 @@ buttons = standard ++ extra
           where
             standard = "qcd=123+456-789*0()/"
             extra = "QCD \ESC\BS\DEL\n"
+
+
+showbox :: IO ()
+showbox = sequence_ [writeat (1,y) b | (y,b) <- zip [1..] box]
+
+display xs = do writeat (3,2) (replicate 13 ' ')
+                writeat (3,2) (reverse (take 13 (reverse xs)))
+
+-- Chapter 10 input/output utilities:
+
+-- clear screen --
+cls :: IO ()
+cls = putStr "\ESC[2J"
+
+type Pos = (Int,Int)
+
+writeat :: Pos -> String -> IO ()
+writeat p xs = do goto p
+                  putStr xs
+
+goto :: Pos -> IO ()
+goto (x,y) = putStr ("\ESC[" ++ show y ++ ";" ++ show x ++ "H")
+
+getCh :: IO Char
+getCh = do hSetEcho stdin False
+           x <- getChar
+           hSetEcho stdin True
+           return x
+
+-------------------------------------------
+
+beep :: IO ()
+beep = putStr "\BEL"
+
+calc :: String -> IO ()
+calc xs = do display xs
+             c <- getCh
+             if elem c buttons then
+                process c xs
+             else
+                do beep
+                   calc xs
+
+process :: Char -> String -> IO ()
+process c xs | elem c "qQ\ESC"    = quit
+             | elem c "dD\BS\DEL" = delete xs
+             | elem c "=\n"       = eval' xs
+             | elem c " cC"       = clear
+             | otherwise          = press c xs
+
+
+-- quitting moves the cursor below the calculator 
+-- box and terminates.
+quit :: IO ()
+quit = goto (1,14)
+
+delete :: String -> IO ()
+delete [] = calc []
+delete xs = calc (init xs)
+
+eval' :: String -> IO ()
+eval' xs = case parse expr xs of 
+             [(n,[])] -> calc (show n)
+             _        -> do beep
+                            calc xs
+
+clear :: IO ()
+clear = calc []
+
+press :: Char -> String -> IO ()
+press c xs = calc (xs ++ [c])
+
+run :: IO ()
+run = do cls
+         showbox
+         clear
+
 
 
 
